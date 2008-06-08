8491bd7c9da5fb4fd69551c67d9d2523a05fb80a
nanki <nanki@c432a1ab-ec09-0410-8e5f-fb625be92370>
2008-06-08 18:46:42 +0000

    * Add Operator to.json.
    git-svn-id: svn+ssh://svn.dotswitch.net/var/svn/dotswitch/projects/library/plugins/purl@1810 c432a1ab-ec09-0410-8e5f-fb625be92370

--- lib/purl/features/conversion.rb
+++ lib/purl/features/conversion.rb
@@ -4,7 +4,8 @@ module Purl
     def self.operators
       [ Op.new('to.png' , 1, :to_png),
         Op.new('to.jpg' , 1, :to_jpeg),
-        Op.new('to.jpeg', 1, :to_jpeg)]
+        Op.new('to.jpeg', 1, :to_jpeg),
+        Op.new('to.json',-1, :to_json)]
     end
 
     def to_png(image)
@@ -19,6 +20,11 @@ module Purl
         img.format = 'jpeg'
         Result.new(img.to_blob{self.quality = 80}, 'image/jpeg')
       end
+    end
+
+    def to_json(*stack)
+      n = stack.pop.to_i
+      Result.new(stack[-n..-1].to_json, 'application/json')
     end
   end
 end
