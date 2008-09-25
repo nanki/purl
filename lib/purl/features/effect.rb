0aa265054e080983a5640725696de818febe8b4c
nanki <nanki@c432a1ab-ec09-0410-8e5f-fb625be92370>
2008-09-25 16:53:30 +0000

    * add operator round.
    git-svn-id: svn+ssh://svn.dotswitch.net/var/svn/dotswitch/projects/library/plugins/purl@2039 c432a1ab-ec09-0410-8e5f-fb625be92370

--- lib/purl/features/effect.rb
+++ lib/purl/features/effect.rb
@@ -1,9 +1,34 @@
 module Purl
   module Features::Effect
+    include ::CairoUtil
     def self.operators
       [ 
+        Op.new(:round, 2),
         Macro.new(:dropshadow, [:swap, :dup, 2, :pull, :shadow, :swap, :composite])
       ]
+    end
+
+    def round(image, r)
+      r = [r, 50].min
+      process(image, :as => :cairo) do |img|
+        img = cairo(img.width, img.height) do |ctx|
+          w = img.width
+          h = img.height
+          c = [r, w / 2, h / 2].min
+          mr = [2 * c - r, r].min
+          
+          ctx.arc c    ,     c, mr,  Math::PI    , -Math::PI / 2
+          ctx.arc w - c,     c, mr, -Math::PI / 2,  0
+          ctx.arc w - c, h - c, mr,  0           ,  Math::PI / 2
+          ctx.arc c    , h - c, mr,  Math::PI / 2,  Math::PI
+          ctx.close_path              
+          ctx.clip
+
+          ctx.set_source(img, 0, 0)
+          ctx.paint
+        end
+        Result.new(img)
+      end
     end
   end
 end
