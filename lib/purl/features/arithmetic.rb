924228dfd1c5e101128864a4a02ba233954e5f21
nanki <nanki@c432a1ab-ec09-0410-8e5f-fb625be92370>
2008-10-09 13:45:47 +0000

    * add xxx.2 arithmetic operators for vector calculation.
    git-svn-id: svn+ssh://svn.dotswitch.net/var/svn/dotswitch/projects/library/plugins/purl@2095 c432a1ab-ec09-0410-8e5f-fb625be92370

--- lib/purl/features/arithmetic.rb
+++ lib/purl/features/arithmetic.rb
@@ -7,7 +7,12 @@ module Purl
           Op.new(:sub, 2),
           Op.new(:mul, 2),
           Op.new(:div, 2),
-          Op.new(:mod, 2)
+          Op.new(:mod, 2),
+          Op.new(:'add.2', 3, :add_2),
+          Op.new(:'sub.2', 3, :sub_2),
+          Op.new(:'mul.2', 3, :mul_2),
+          Op.new(:'div.2', 3, :div_2),
+          Op.new(:'mod.2', 3, :mod_2),
         ]
       end
 
@@ -29,6 +34,27 @@ module Purl
 
       def mod(op1, op2)
         Result.new(op1 % op2)
+      end
+
+      # for vector
+      def add_2(op1, op2, num)
+        Result.new(op1 + num, op2 + num)
+      end
+
+      def sub_2(op1, op2, num)
+        Result.new(op1 - num, op2 - num)
+      end
+
+      def mul_2(op1, op2, num)
+        Result.new(op1 * num, op2 * num)
+      end
+
+      def div_2(op1, op2, num)
+        Result.new(op1.quo(num), op2.quo(num))
+      end
+
+      def mod_2(op1, op2, num)
+        Result.new(op1 % num, op2 % num)
       end
     end
   end
