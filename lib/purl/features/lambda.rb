9d1f442f6076ca285aeaaef3c39cc666a1ded1fb
nanki <nanki@c432a1ab-ec09-0410-8e5f-fb625be92370>
2008-10-28 22:06:28 +0000

    * add each.n operator.
    git-svn-id: svn+ssh://svn.dotswitch.net/var/svn/dotswitch/projects/library/plugins/purl@2124 c432a1ab-ec09-0410-8e5f-fb625be92370

--- lib/purl/features/lambda.rb
+++ lib/purl/features/lambda.rb
@@ -8,6 +8,7 @@ module Purl
           Op.new(:call, 1),
           Op.new(:times, 2),
           Op.new(:each, -1),
+          Op.new(:'each.n', -1, 'each_n'),
         ]
       end
 
@@ -26,6 +27,21 @@ module Purl
         op = macro.dispatch
 
         stack.concat stack.slice!(-n, n).map{|v| [v, *op]}.flatten
+        Result.new(*stack)
+      end
+
+      def each_n(*stack)
+        n = [@max_n, stack.pop].min
+        m = [@max_n, stack.pop].min
+        macro = stack.pop
+
+        op = macro.dispatch
+
+        mn = m * n
+        array = stack.slice!(- mn, mn)
+
+        stack.concat((0...n).map{[array.slice!(0, m), *op]}.flatten)
+
         Result.new(*stack)
       end
     end
