e28d20e4c625059336a0b310c3adffddfb673803
nanki <nanki@c432a1ab-ec09-0410-8e5f-fb625be92370>
2008-10-30 08:42:57 +0000

    * add Gayzo module.  * import images from gyazo.com.
    git-svn-id: svn+ssh://svn.dotswitch.net/var/svn/dotswitch/projects/library/plugins/purl@2130 c432a1ab-ec09-0410-8e5f-fb625be92370

---
+++ lib/purl/features/gyazo.rb
@@ -0,0 +1,37 @@
+require 'open-uri'
+require 'fileutils'
+
+module Purl
+  module Features::Gyazo
+    class << self
+      def operators
+        [Op.new(:gyazo, 1)]
+      end
+
+      attr_reader :cache_path
+      def options=(options)
+        options = {
+          :cache_path => File.join(RAILS_ROOT, 'public', 'purl'),
+        }.merge(options)
+
+        @cache_path = options[:cache_path]
+      end
+
+      def gyazo(id)
+        id.gsub!(/[^a-f0-9]/, '')
+
+        path = File.join(@cache_path, "(#{id}):gyazo:to.png")
+
+        unless File.exist?(path)
+          FileUtils.makedirs(@cache_path)
+          data = open("http://gyazo.com/#{id}.png").read
+          open(path, "wb+"){|file| file.write(data)}
+        else
+          data = open(path).read
+        end
+
+        Result.new(Magick::Image.from_blob(data).shift.strip!)
+      end
+    end
+  end
+end
