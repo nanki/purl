2b813248e05e643131a621ef9100b51aeccc5f98
nanki <nanki@c432a1ab-ec09-0410-8e5f-fb625be92370>
2008-11-19 20:54:14 +0000

    * .2 operator(add.2 etc...) is obsolete.  * 1:2:2:add.2 is equivalent to 1:2:[:2:add:]:2:each
    git-svn-id: svn+ssh://svn.dotswitch.net/var/svn/dotswitch/projects/library/plugins/purl@2163 c432a1ab-ec09-0410-8e5f-fb625be92370

--- lib/purl/features/arithmetic.rb
+++ lib/purl/features/arithmetic.rb
@@ -8,11 +8,6 @@ module Purl
           Op.new(:mul, 2),
           Op.new(:div, 2),
           Op.new(:mod, 2),
-          Op.new('add.2', 3, :add_2),
-          Op.new('sub.2', 3, :sub_2),
-          Op.new('mul.2', 3, :mul_2),
-          Op.new('div.2', 3, :div_2),
-          Op.new('mod.2', 3, :mod_2),
         ]
       end
 
@@ -35,26 +30,39 @@ module Purl
       def mod(op1, op2)
         Result.new(op1 % op2)
       end
+    end
 
-      # for vector
-      def add_2(op1, op2, num)
-        Result.new(op1 + num, op2 + num)
-      end
+    module Obsolete
+      class << self
+        def operators
+          [
+            Op.new('add.2', 3, :add_2),
+            Op.new('sub.2', 3, :sub_2),
+            Op.new('mul.2', 3, :mul_2),
+            Op.new('div.2', 3, :div_2),
+            Op.new('mod.2', 3, :mod_2),
+          ]
+        end
 
-      def sub_2(op1, op2, num)
-        Result.new(op1 - num, op2 - num)
-      end
+        def add_2(op1, op2, num)
+          Result.new(op1 + num, op2 + num)
+        end
 
-      def mul_2(op1, op2, num)
-        Result.new(op1 * num, op2 * num)
-      end
+        def sub_2(op1, op2, num)
+          Result.new(op1 - num, op2 - num)
+        end
 
-      def div_2(op1, op2, num)
-        Result.new(op1.quo(num), op2.quo(num))
-      end
+        def mul_2(op1, op2, num)
+          Result.new(op1 * num, op2 * num)
+        end
 
-      def mod_2(op1, op2, num)
-        Result.new(op1 % num, op2 % num)
+        def div_2(op1, op2, num)
+          Result.new(op1.quo(num), op2.quo(num))
+        end
+
+        def mod_2(op1, op2, num)
+          Result.new(op1 % num, op2 % num)
+        end
       end
     end
   end
